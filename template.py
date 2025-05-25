f209fa1955926c57afbc1abae72dfa5f8158454d
Shubham Jagtap <shubhamjagtap@Shubhams-MacBook-Air-2.local>
2025-05-25 16:43:36 -0400

    folder structure added

--- template.py
+++ template.py
@@ -18,7 +18,7 @@ list_of_files = [
     f"src/{project_name}/constants/__init__.py",
     "config/congif.yaml",
     "dvc.yaml",
-    "params.yaml"
+    "params.yaml",
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb"
